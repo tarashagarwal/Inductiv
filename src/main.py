c9f2b4c2d66128f2079e890368e8ac695b0f4233
Tarash Agarwal <tarash.agarwal@gmail.com>
2025-04-26 12:26:50 -0500

    updates

--- src/main.py
+++ src/main.py
@@ -1,41 +1,5 @@
 from transformers import pipeline
 
-# classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-
-# labels = ["general", "course-related"]
-
-# query = "Hello, Did not see you in class today?"
-
-# result = classifier(query, candidate_labels=labels)
-# print(result)
-
-
-# 1. Load the Roberta Large NER Model
-# ner_pipeline = pipeline(
-#     "ner",
-#     model="dbmdz/bert-large-cased-finetuned-conll03-english",
-#     aggregation_strategy="simple"  # This merges multi-token entities nicely
-# )
-
-# # 2. Define a user query
-# query = "Professor John Smith teaches Data Mining at Stanford University."
-
-# # 3. Run NER
-# entities = ner_pipeline(query)
-
-# # 4. Print the detected entities
-# for entity in entities:
-#     print(f"Entity: {entity['word']} | Label: {entity['entity_group']} | Score: {entity['score']:.2f}")
-
-
-# classifier = pipeline("zero-shot-classification", model="tarashagarwal/inductiv-binary-classifier")
-
-# candidate_labels = ["course-related", "general", "sports", "weather"]
-# sequence_to_classify = "Can you give description for CSCI2200?"
-
-# result = classifier(sequence_to_classify, candidate_labels)
-# print(result)
-
 
 classifier = pipeline(
     "text-classification",
@@ -47,14 +11,25 @@ def test_query(text):
     result = classifier(text)[0]
     label = result["label"]
     score = result["score"]
-    print(f"Prediction: {label}, Confidence: {score:.2f}")
+    print(f"Prediction: {label}, Confidence: {score}")
+    print("asasas:" ,float(result['score']) > 0.50)
+    return result["label"] if float(result["score"]) > 0.50  else  reverseLabel(result["label"])
 
-test_query("Who teaches Machine Learning this fall?")
-test_query("What's the weather like in Paris?")
-test_query("Guruparan, A;Mukhopadhyay si the instructor for Fall for BADM2200?")
-test_query("Asish K for Fall for BADM2200? in summer")
-test_query("What is the univeristy ranking?")
-test_query("What are some some hot topics?")
-test_query("Is BADM576 taught this semester?")
+def reverseLabel(label):
+    return "LABEL_0" if label == "LABEL_1" else "LABEL_1"
 
 
+# test_query("Who teaches Machine Learning this fall?")
+# test_query("What's the weather like in Paris?")
+# test_query("Guruparan, A;Mukhopadhyay si the instructor for Fall for BADM2200?")
+# test_query("Asish K for Fall for BADM2200? in summer")
+# test_query("What is the univeristy ranking?")
+# test_query("What are some some hot topics?")
+# test_query("Is BADM576 taught this semester?")
+
+while True:
+    prompt = input("Hpw can I help?\n")
+    if prompt == "stop":
+        exit()
+    else:
+        print(test_query(prompt))
